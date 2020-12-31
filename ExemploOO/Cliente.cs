46b6936a92033c0cf873b47de91fb74117a04b1a
wslmacieiradev <wslmacieira@outlook.com>
2020-12-31 09:26:47 -0300

    adiciona classe Cliente

--- ExemploOO/Cliente.cs
+++ ExemploOO/Cliente.cs
@@ -1,13 +1,50 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace ExemploOO
 {
     public class Cliente
     {
+        public Cliente(int codigo, string nome,
+            DateTime dataDeNascimento, string userName,
+            string senha, string email, string cpf)
+        {
+            Codigo = codigo;
+            Nome = nome;
+            DataDeNascimento = dataDeNascimento;
+            UserName = userName;
+            Senha = GerarHashDaSenha(senha);
+            Email = email;
+            Cpf = cpf;
+            DataDeCadastro = DateTime.Now;
+
+            if (Codigo == 0)
+            {
+                throw new ApplicationException("Código inválido");
+            }
+
+            if (string.IsNullOrEmpty(Nome) || Nome.Length < 3)
+            {
+                throw new ApplicationException("Nome inválido");
+            }
+
+            if (Cpf.Length != 11)
+            {
+                throw new ApplicationException("Cpf inválido");
+            }
+        }
+
         public int Codigo { get; private set; }
         public string Nome { get; private set; }
+        public DateTime DataDeNascimento { get; private set; }
+        public string UserName { get; private set; }
+        public string Senha { get; private set; }
+        public string Email { get; private set; }
+        public string Cpf { get; private set; }
+        public DateTime DataDeCadastro { get; private set; }
 
+        private string GerarHashDaSenha(string senha)
+        {
+            return senha;
+        }
     }
 }
