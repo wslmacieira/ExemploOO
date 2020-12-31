df9c970ca13308885bc06bf93f4bc3fc489094e1
wslmacieiradev <wslmacieira@outlook.com>
2020-12-31 09:12:02 -0300

    Adicionar arquivos de projeto.

---
+++ ExemploOO/Cliente.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExemploOO
+{
+    public class Cliente
+    {
+        public int Codigo { get; private set; }
+        public string Nome { get; private set; }
+
+    }
+}
